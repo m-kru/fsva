4e1d30c7a77c165d83e19caeb6c75220da79f097
Michał Kruszewski <mkru@protonmail.com>
2019-08-14 17:41:14 +0200

    First working version. Needs improvements!

---
+++ src/lib.rs
@@ -0,0 +1,257 @@
+use std::env::{current_dir, set_current_dir};
+use std::error::Error;
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::process;
+
+use clap::ArgMatches;
+use serde_yaml as sy;
+use chrono::prelude::Utc;
+
+pub fn run(config: ArgMatches) -> Result<(), Box<dyn Error>> {
+    let workpath = string_to_path(config.value_of("workpath").unwrap().to_string());
+    set_current_dir(&workpath)?;
+
+    let core_files = find_core_files(&workpath)?;
+    println!("{:?}", core_files);
+
+    if core_files.is_empty() {
+        return Err(format!("No .core files found under the workpath: {:?}.", workpath).into());
+    }
+
+    let mut verification_targets = vec![];
+    for file in core_files {
+        verification_targets.append(&mut VerificationTarget::new(&file)?);
+    }
+
+    if verification_targets.is_empty() {
+        return Err(format!("No verification targets found.").into());
+    }
+    println! {"{:#?}", verification_targets};
+
+    let date_utc = Utc::now()
+                    .format("%Y-%m-%d_%H:%M:%S")
+                    .to_string();
+
+    let outpath: PathBuf = workpath.join(config.value_of("outdir").unwrap().to_string())
+                    .join(date_utc);
+    println!("{:?}", outpath);
+    fs::create_dir_all(&outpath)?;
+
+    for target in &mut verification_targets {
+        target.prepare_for_verification(&outpath);
+    }
+
+    for target in &mut verification_targets {
+        target.verify()?;
+    }
+    println! {"{:#?}", verification_targets};
+
+    summarize(verification_targets)?;
+
+    Ok(())
+}
+
+fn summarize(verification_targets: Vec<VerificationTarget>) -> Result<(), &'static str> {
+    let num_targets = verification_targets.len();
+    let num_passed = verification_targets.iter()
+                            .filter(|x| x.passed)
+                            .count();
+    let num_failed = num_targets - num_passed;
+
+    let mut all_passed = true;
+    let mut num_warnings = 0;
+
+    for target in verification_targets {
+        if !target.passed {
+            all_passed = false;
+        }
+
+        if target.number_of_warnings > 0 {
+            println!("Warnings found for core: {}, target: {}", target.core_name, target.target_name);
+            println!("For more details check file: {:?}", target.output_file);
+
+            num_warnings += target.number_of_warnings;
+        }
+    }
+
+
+    println!("VERIFICATION SUMMARY:");
+    println!("TARGETS:  {}", num_targets);
+    println!("PASSED:   {} ({:.2}%)",
+        num_passed,
+        num_passed as f32 / num_targets as f32 * 100.0);
+    println!("FAILED:   {} ({:.2}%)",
+        num_failed,
+        num_failed as f32 / num_targets as f32 * 100.0);
+    println!("WARNINGS: {}", num_warnings);
+
+    if !all_passed {
+        return Err("At least one verification target failed. Check summary for details.");
+    }
+
+    Ok(())
+}
+
+#[derive(Clone, Debug)]
+//#[derive(Debug)]
+struct VerificationTarget {
+    core_file: PathBuf,
+    core_name: String,
+    target_name: String,
+    eda_tool: String,
+    output_file: PathBuf,
+    // Attributes for verification command.
+    command: String,
+    command_arguments: Vec<String>,
+    // Attributes for verification results.
+    passed: bool,
+    number_of_warnings: u32,
+    // TODO: add attribute with time for verification
+}
+
+impl Default for VerificationTarget {
+    fn default() -> VerificationTarget {
+        VerificationTarget {
+            core_file: PathBuf::new(),
+            core_name: String::new(),
+            target_name: String::new(),
+            eda_tool: String::new(),
+            output_file: PathBuf::new(),
+            command: String::from("fusesoc"),
+            command_arguments: vec![String::from("--cores-root"),
+                                    String::from("."),
+                                    String::from("run"),
+                                    String::from("--target")],
+            passed: false,
+            number_of_warnings: 0,
+        }
+    }
+}
+
+impl VerificationTarget {
+    // Return Vec as single .core file can contain multiple verification targets.
+    fn new(file: &PathBuf) -> Result<Vec<VerificationTarget>, Box<dyn Error>> {
+        let mut ver_targets = vec![];
+
+        let f = fs::File::open(file)?;
+
+        let yaml: sy::Value = sy::from_reader(f)?;
+
+        let core_name;
+        match yaml.get("name").unwrap() {
+            sy::Value::String(s) => core_name = s,
+            _ => return Err(format!("core name must be string, file: {:?}", file).into()),
+        }
+
+        if let sy::Value::Mapping(m) = yaml.get("targets").unwrap() {
+            for target in m {
+
+                if let sy::Value::String(target_name) = target.0 {
+                    if target_name.starts_with("tb_") || target_name.ends_with("_tb") {
+                        let tool = target.1.get("default_tool");
+                        let mut eda_tool = String::from("");
+                        if tool.is_some() {
+                            if let sy::Value::String(s) = tool.unwrap() {
+                                eda_tool = s.to_string();
+                            }
+                        }
+
+                        let ver_target = VerificationTarget {
+                            core_file: file.to_path_buf(),
+                            core_name: core_name.clone(),
+                            target_name: target_name.clone(),
+                            eda_tool: eda_tool.clone(),
+                            ..VerificationTarget::default()};
+                        ver_targets.push(ver_target);
+                    }
+                }
+            }
+        }
+
+        Ok(ver_targets)
+    }
+
+    fn prepare_for_verification(&mut self, outpath: &PathBuf) {
+        //let mut file_name = self.core_name.clone().replace(":","-");
+        //let file_name = self.core_name.clone().trim_start_matches('c').to_owned();
+        let file_name = self.core_name.trim_start_matches(':')
+                            .replace(":", "-") + "_" + &self.target_name;
+        println!("{}", file_name);
+        //self.output_file = outpath.join(self.core_name.clone() + &self.target_name.clone());
+        self.output_file = outpath.join(file_name);
+
+        self.command_arguments.push(self.target_name.clone());
+        self.command_arguments.push(self.core_name.clone());
+    }
+
+        fn verify(&mut self) -> io::Result<()> {
+            let output = process::Command::new(self.command.clone())
+                .args(self.command_arguments.clone())
+                .output()?;
+
+            if output.status.success() {
+                self.passed = true;
+            }
+
+            let mut file = fs::File::create(self.output_file.clone())?;
+            file.write_all(b"***** STANDARD ERROR *****\n\n")?;
+            file.write_all(&output.stderr)?;
+
+            file.write_all(b"\n***** STANDARD OUTPUT *****\n\n")?;
+            file.write_all(&output.stdout)?;
+
+            println!("{:#?}", output);
+            Ok(())
+        }
+}
+
+pub fn string_to_path(val: String) -> PathBuf {
+    let mut path = PathBuf::from(val);
+
+    if !path.is_absolute() {
+        path = current_dir().unwrap().join(path);
+    }
+
+    path
+}
+
+fn find_core_files(dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
+    let mut core_files = vec![];
+
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                core_files.append(&mut find_core_files(&path)?);
+            } else {
+                match path.extension() {
+                    Some(e) => {
+                        if e == "core" {
+                            core_files.push(path);
+                        }
+                    }
+                    _ => (),
+                }
+            }
+        }
+    }
+
+    Ok(core_files)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn string_to_path_absolute() {
+        let s = String::from("/tmp/abc/def");
+        let p = PathBuf::from("/tmp/abc/def");
+
+        assert_eq!(p, string_to_path(s));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use std::process;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let config = App::new("FuseSoc Verification Automation")
+        .about(
+            "Tool for automating verification process for HDL projects using FuseSoc build tool."
+        )
+        .version("0.1.0")
+        .author("Michał Kruszewski mkru@protonmail.com")
+        .arg(
+            Arg::with_name("workpath")
+                .long("workpath")
+                .short("w")
+                .help("Work path. Path to recursively look for FuseSoc .core files.")
+                .takes_value(true)
+                .default_value(".")
+                .validator(workpath_validator)
+        )
+        .arg(
+            Arg::with_name("outdir")
+                .long("outdir")
+                .short("o")
+                .help("Output directory name. This directory is created in workpath.")
+                .takes_value(true)
+                .default_value("_fsva")
+        )
+        .arg(
+            Arg::with_name("numprocesses")
+                .long("numprocesses")
+                .short("n")
+                .help("Number of processes allowed to be spawn in parallel.")
+                .takes_value(true)
+                .default_value("1")
+        )
+        .arg(
+            Arg::with_name("compress")
+                .long("compress")
+                .short("c")
+                .help("Automatically compress output directory if all tests pass.")
+        )
+        .get_matches();
+
+    let output = process::Command::new("fusesoc")
+        .arg("--version")
+        .output()
+        .expect("Failed to launch fusesoc, is it installed?");
+    print!("Found fusesoc version {}", String::from_utf8(output.stdout).unwrap());
+
+    if let Err(e) = fsva::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn workpath_validator(val: String) -> Result<(), String> {
+    let workpath = fsva::string_to_path(val);
+
+    if workpath.exists() {
+        return Ok(())
+    }
+
+    Err(format!("{:?} workpath does not exist.", workpath))
 }
